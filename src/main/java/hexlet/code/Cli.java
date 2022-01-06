4b800e7fe71304e98313df188f8fef0e82e424ed
qed <qedirast@gmail.com>
2022-01-06 18:01:58 +0700

    Added greeting

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void greeting() {
+        System.out.print("May I have your name? ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println (String.format("Hello, %s!", name));
+    }
+}
