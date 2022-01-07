873f4d89eb2b335fbdbaff1fc3d4150dab4c8e06
qed <qedirast@gmail.com>
2022-01-07 20:14:23 +0700

    fix isPrime

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -43,6 +43,10 @@ public class Prime {
     }
 
     private static boolean isPrime(int number) {
+        if (number >= 0 && number < 2) {
+            return false;
+        }
+
         for (var i = 2; i < number / 2; i++) {
             if (number % i == 0) {
                 return false;
